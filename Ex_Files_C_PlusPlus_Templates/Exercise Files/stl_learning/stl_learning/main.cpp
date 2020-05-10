c2fc1c02f6fd0cf0c1b5937d9034e1b44cf33d97
gilson27 <gilsonvarghese7@gmail.com>
2020-05-10 18:47:27 +0530

    Added deep learning image files

---
+++ Ex_Files_C_PlusPlus_Templates/Exercise Files/stl_learning/stl_learning/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  stl_learning
+//
+//  Created by gilson on 02/05/20.
+//  Copyright © 2020 gilson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
