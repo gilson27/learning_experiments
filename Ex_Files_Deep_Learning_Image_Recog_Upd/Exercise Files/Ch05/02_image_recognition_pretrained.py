121b37053d57cb797dfa8b2bd1ab40fc979757df
gilson27 <gilsonvarghese7@gmail.com>
2020-05-12 23:23:31 +0530

    used vgg16 for img classification

--- Ex_Files_Deep_Learning_Image_Recog_Upd/Exercise Files/Ch05/02_image_recognition_pretrained.py
+++ Ex_Files_Deep_Learning_Image_Recog_Upd/Exercise Files/Ch05/02_image_recognition_pretrained.py
@@ -3,7 +3,7 @@ from keras.preprocessing import image
 from keras.applications import vgg16
 
 # Load Keras' VGG16 model that was pre-trained against the ImageNet database
-model =
+model = vgg16.VGG16()
 
 # Load the image file, resizing it to 224x224 pixels (required by this model)
 img = image.load_img("bay.jpg", target_size=(224, 224))
@@ -15,13 +15,13 @@ x = image.img_to_array(img)
 x = np.expand_dims(x, axis=0)
 
 # Normalize the input image's pixel values to the range used when training the neural network
-x =
+x = vgg16.preprocess_input(x)
 
 # Run the image through the deep neural network to make a prediction
-predictions =
+predictions = model.predict(x)
 
 # Look up the names of the predicted classes. Index zero is the results for the first image.
-predicted_classes =
+predicted_classes = vgg16.decode_predictions(predictions)
 
 print("Top predictions for this image:")
 
